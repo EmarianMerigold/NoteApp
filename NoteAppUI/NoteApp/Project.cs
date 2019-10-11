3c20f7fd760089bc57cc9804f31bad6f0dc213b5
Dyachenko K <kostyan.tsk@gmail.com>
2019-10-11 11:55:57 +0700

    Реализовал словарь ( project.cs)

---
+++ NoteAppUI/NoteApp/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Класс, содержащий в себе стуктуру данных "Словарь".
+    /// </summary>
+    class Project
+    {
+        Dictionary<int, Note> dictionary = new Dictionary<int, Note>();
+    }
+}
